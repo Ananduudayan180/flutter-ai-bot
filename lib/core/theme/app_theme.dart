3756e07147d62636a29586b7eb85d82583544f1d
Anandu Udayan <anandhuudayan180@gamil.com>
2026-08-20 11:07:44 +0530

    feat(theme): add text theme configuration

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ai_bot/core/theme/color_theme.dart';
+import 'package:flutter_ai_bot/core/theme/text_theme.dart';
 
 final darkTheme = ThemeData(
+  textTheme: textTheme,
   scaffoldBackgroundColor: Color(0xFF000000),
   colorScheme: darkColorScheme,
   appBarTheme: AppBarTheme(
@@ -12,6 +14,7 @@ final darkTheme = ThemeData(
 );
 
 final lightTheme = ThemeData(
+  textTheme: textTheme,
   scaffoldBackgroundColor: Colors.white,
   colorScheme: lightColorScheme,
   appBarTheme: AppBarTheme(
