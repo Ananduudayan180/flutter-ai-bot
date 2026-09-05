b320a6eef9bf1af6a023133170e280c1b1107eff
Anandu Udayan <anandhuudayan180@gmail.com>
2026-09-05 12:24:07 +0530

    feat(app): initialize main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ai_bot/core/theme/app_theme.dart';
+import 'package:flutter_ai_bot/features/onboarding/presentation/screens/onboarding_view.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Ai App',
+      theme: darkTheme,
+      home: const OnboardingView(),
+    );
+  }
+}
