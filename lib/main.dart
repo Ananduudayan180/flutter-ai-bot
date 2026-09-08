9cb7d6b9150923f5ee804ff89dfdef566d2ab5f9
Anandu Udayan <anandhuudayan180@gmail.com>
2026-09-08 12:18:28 +0530

    chore: add .env and load environment variables in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ai_bot/core/theme/app_theme.dart';
 import 'package:flutter_ai_bot/core/theme/theme_mode_notifier.dart';
 import 'package:flutter_ai_bot/features/onboarding/presentation/screens/onboarding_view.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(ProviderScope(child: const MyApp()));
 }
 
